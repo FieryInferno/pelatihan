089c7f4f22bcc6ce7f354cd13c80058ac36f7a4c
FieryInferno <bagassetia271@gmail.com>
2023-08-06 13:03:38 +0700

    fix:materi context

--- src/components/MateriCard.tsx
+++ src/components/MateriCard.tsx
@@ -7,13 +7,13 @@ import { MateriContext } from "@/data/MateriProvider";
 import { useRouter } from "next/navigation";
 
 export default (materi : {image: StaticImageData, title: string}) => {
-    const { setMateri } = useContext(MateriContext);
+    const { newSetMateri } = useContext(MateriContext);
     const router = useRouter();
 
     return (
         <>
             <div onClick={() => {
-                setMateri(materi);
+                newSetMateri(materi);
                 router.push('materi');
             }} className="cursor-pointer">
                 <div className="p-4 bg-[rgb(27,29,30)] flex flex-col justify-center items-center border border-[rgb(57,61,64)] shadow-[rgba(82,98,119,0.5)_0_4px_0] transition-all duration-300 hover:-translate-y-2 h-[200px] rounded-lg">
--- src/data/MateriProvider.tsx
+++ src/data/MateriProvider.tsx
@@ -4,11 +4,15 @@ import { ReactNode, createContext, useState } from "react";
 
 export const MateriContext = createContext<any>({});
 export default ({ children } : {children: ReactNode}) => {
-    const [materi, setMateri] = useState<string | null>(null);
-    
+    const [materi, setMateri] = useState<object | null>(JSON.parse(localStorage.getItem('materi') as string));
+    const newSetMateri = (materi: object) => {
+        setMateri(materi);
+        localStorage.setItem('materi', JSON.stringify(materi));
+    }
+
     return (
         <>
-            <MateriContext.Provider value={{materi, setMateri}}>{children}</MateriContext.Provider>
+            <MateriContext.Provider value={{materi, newSetMateri}}>{children}</MateriContext.Provider>
         </>
     );
 }
