b53d482e46c8d4a301ccf0378532e6528304b9fa
FieryInferno <bagassetia271@gmail.com>
2023-08-06 15:14:39 +0700

    localStorage

--- src/data/MateriProvider.tsx
+++ src/data/MateriProvider.tsx
@@ -1,14 +1,20 @@
 'use client'
 
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useState, useEffect } from "react";
 
 export const MateriContext = createContext<any>({});
 export default ({ children } : {children: ReactNode}) => {
-    const [materi, setMateri] = useState<object | null>(JSON.parse(localStorage.getItem('materi') as string));
+    const [materi, setMateri] = useState<object | null>(null);
     const newSetMateri = (materi: object) => {
         setMateri(materi);
         localStorage.setItem('materi', JSON.stringify(materi));
     }
+
+    useEffect(() => {
+        const storedMateri = localStorage.getItem('materi');
+
+        if (storedMateri) setMateri(JSON.parse(storedMateri));
+    }, []);
 
     return (
         <>
